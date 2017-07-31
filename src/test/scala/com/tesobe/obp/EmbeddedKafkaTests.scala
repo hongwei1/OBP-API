295c1d64a4c882f8e50f8d01c7aeb3ee94846619
jota <jota@mailbox.tu-berlin.de>
2017-07-31 16:06:25 +0200

    fixed EmbeddedKafkaTests Stub

--- src/test/scala/com/tesobe/obp/EmbeddedKafkaTests.scala
+++ src/test/scala/com/tesobe/obp/EmbeddedKafkaTests.scala
@@ -3,8 +3,5 @@ package com.tesobe.obp
 import net.manub.embeddedkafka.EmbeddedKafka
 import org.scalatest._
 
-class EmbeddedKafkaTests extends FlatSpec with EmbeddedKafka { "runs with embedded kafka" should { withRunningKafka {
-  
-  // test cases goes here 
-   } } }
+class EmbeddedKafkaTests extends FlatSpec with EmbeddedKafka { }
 
