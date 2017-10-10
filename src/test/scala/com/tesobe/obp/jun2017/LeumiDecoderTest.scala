f37bc41a0128090d8a437a1a5479e508cfb2cf92
tawoe <jota@mailbox.tu-berlin.de>
2017-10-10 12:32:09 +0200

    fixed LeumiDecoderTests

--- src/test/scala/com/tesobe/obp/jun2017/LeumiDecoderTest.scala
+++ src/test/scala/com/tesobe/obp/jun2017/LeumiDecoderTest.scala
@@ -13,31 +13,32 @@ class LeumiDecoderTest  extends ServerSetup {
   val accountId3 = base64EncodedSha256("50180963" + "fjdsaFDSAefwfsalfid")
   val accountId4 = base64EncodedSha256("20102612" + "fjdsaFDSAefwfsalfid")
   val accountId5 = base64EncodedSha256("20105505" + "fjdsaFDSAefwfsalfid")
+  val mfToken = "?+1         81433020102612"
   
   test("getBankAccounts works for Stub"){
     val result = getBankAccounts(OutboundGetAccounts(AuthInfo("karlsid", "karl", ""), null)) //TODO ,need fix
 
     //Balance is from nt1c call, all accounts use the same json stub => all accounts have the same balance
-    result should be (InboundBankAccounts(AuthInfo("karlsid", "karl", ">,?          81433020102612"),
-      List(InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), ">,?          81433020102612", "10", "616", accountId1, "3565953", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""),
-        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), ">,?          81433020102612", "10", "616", accountId2, "50180983", "430", "5541.28", "ILS", List("karl"), List("Owner"), "", "", "", "", "", ""), 
-        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), ">,?          81433020102612", "10", "616", accountId3, "50180963", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""),
-        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), ">,?          81433020102612", "10", "814", accountId4, "20102612", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""),
-        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), ">,?          81433020102612", "10", "814", accountId5, "20105505", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""))))
+    result should be (InboundBankAccounts(AuthInfo("karlsid", "karl", mfToken),
+      List(InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), mfToken, "10", "616", accountId1, "3565953", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""),
+        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), mfToken, "10", "616", accountId2, "50180983", "430", "5541.28", "ILS", List("karl"), List("Owner"), "", "", "", "", "", ""), 
+        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), mfToken, "10", "616", accountId3, "50180963", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""),
+        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), mfToken, "10", "814", accountId4, "20102612", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""),
+        InboundAccountJune2017("", List(InboundStatusMessage("ESB","Success", "0", "OK")), mfToken, "10", "814", accountId5, "20105505", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""))))
   }
   
   test("getBankAccountbyAccountId works for Stub"){
-    val result = getBankAccountbyAccountId(GetAccountbyAccountID(AuthInfo("karlsid", "karl", ">,?          81433020102612"),"10",accountId1))
-    result should be (InboundBankAccount(AuthInfo("karlsid", "karl", ">,?          81433020102612"),(InboundAccountJune2017("",List(InboundStatusMessage("ESB","Success", "0", "OK")),  ">,?          81433020102612", "10", "616", accountId1, "3565953", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""))))
+    val result = getBankAccountbyAccountId(GetAccountbyAccountID(AuthInfo("karlsid", "karl", mfToken),"10",accountId1))
+    result should be (InboundBankAccount(AuthInfo("karlsid", "karl", mfToken),(InboundAccountJune2017("",List(InboundStatusMessage("ESB","Success", "0", "OK")),  mfToken, "10", "616", accountId1, "3565953", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""))))
   }
 
   test("getBankAccountbyAccountNumber works for Stub"){
-    val result = getBankAccountByAccountNumber(GetAccountbyAccountNumber(AuthInfo("karlsid", "karl", ">,?          81433020102612"),"10","3565953"))
-    result should be (InboundBankAccount(AuthInfo("karlsid", "karl", ">,?          81433020102612"),(InboundAccountJune2017("",List(InboundStatusMessage("ESB","Success", "0", "OK")),  ">,?          81433020102612", "10", "616", accountId1, "3565953", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""))))
+    val result = getBankAccountByAccountNumber(GetAccountbyAccountNumber(AuthInfo("karlsid", "karl", mfToken),"10","3565953"))
+    result should be (InboundBankAccount(AuthInfo("karlsid", "karl", mfToken),(InboundAccountJune2017("",List(InboundStatusMessage("ESB","Success", "0", "OK")),  mfToken, "10", "616", accountId1, "3565953", "330", "5541.28", "ILS", List(""), List("Auditor"), "", "", "", "", "", ""))))
   }
   
   test("getTransactions works for Stubs first transaction"){
-    val result = getTransactions(GetTransactions(AuthInfo("karlsid", "karl", ""), "10", accountId1, 15, "", ""))
+    val result = getTransactions(GetTransactions(AuthInfo("karlsid", "karl", ""), "10", accountId1, 15, "Sat Jul 01 00:00:00 CEST 2000", "Sat Jul 01 00:00:00 CEST 2017"))
     val transactionId = base64EncodedSha256(result.data.head.amount + result.data.head.completedDate + result.data.head.newBalanceAmount)
     result.data.head should be (InternalTransaction(
       "",
@@ -64,7 +65,7 @@ class LeumiDecoderTest  extends ServerSetup {
   
   test("getToken gives correct token") {
     val result = getToken(GetToken("N7jut8d"))
-    result should be (InboundToken("N7jut8d",">,?          81433020102612"))
+    result should be (InboundToken("N7jut8d",mfToken))
   }
 
 
