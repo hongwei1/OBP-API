8d31467e4ba7b1ccb99872437fceb0b20ac1d626
tawoe <jota@mailbox.tu-berlin.de>
2017-11-14 17:25:46 +0100

    initial commit

--- src/main/scala/com/tesobe/obp/mf_calls/GetBankAccounts.scala
+++ src/main/scala/com/tesobe/obp/mf_calls/GetBankAccounts.scala
@@ -3,7 +3,7 @@ package com.tesobe.obp
 import java.security.MessageDigest
 
 import com.tesobe.obp.ErrorMessages._
-import com.tesobe.obp.JoniMf.{correctArrayWithSingleElement, getJoniMfHttpApache, replaceEmptyObjects}
+import com.tesobe.obp.JoniMf.getJoniMf
 import com.tesobe.obp.june2017.LeumiDecoder.cachedJoni
 import net.liftweb.json.JValue
 import net.liftweb.json.JsonParser.parse
@@ -14,45 +14,42 @@ import scala.collection.mutable.ListBuffer
 
 object GetBankAccounts extends Config {
 
-  def getBasicBankAccountsForUser(username: String, useCache: Boolean): List[BasicBankAccount] = {
-    //Simulating mainframe call
-    implicit val formats = net.liftweb.json.DefaultFormats
-    //Creating JSON AST
-    val json: String = useCache match {
-      case true => cachedJoni.get(username).getOrElse(throw new JoniCacheEmptyException(s"$JoniCacheEmpty The Joni Cache Input Key =$username "))
-      case false => 
-        val result = getJoniMfHttpApache(username)
-        if (result.contains("PAPIErrorResponse")) throw new JoniFailedException(s"$JoniFailed Current Response is $result") else
-        cachedJoni.set(username, result)
-        cachedJoni.get(username).getOrElse(throw new JoniCacheEmptyException(s"$JoniCacheEmpty The Joni Cache Input Key =$username "))
-    }
-    val jsonAst: JValue = correctArrayWithSingleElement(parse(replaceEmptyObjects(json)))
-    //Create case class object JoniMfUser
-    val jsonExtract: JoniMfUser = jsonAst.extract[JoniMfUser]
-    val allowedAccountTypes = config.getString("accountTypes.allowed").split(",").map(_.trim).toList
-    //now extract values from JoniMFUser object into List of BasicBankAccount
-    // (will be changed to inboundBankAccount 2017 objects 
-    var result = new ListBuffer[BasicBankAccount]()
+  def getBasicBankAccountsForUser(username: String, useCache: Boolean): Either[PAPIErrorResponse, List[BasicBankAccount]] = {
 
-    for ( i <- jsonExtract.SDR_JONI.SDR_LAK_SHEDER.SDRL_LINE) {
-      for (u <- i.SDR_CHN) {
-        if (allowedAccountTypes.contains(u.SDRC_LINE.SDRC_CHN.SDRC_CHN_SUG)) {
-          val hasMursheMeida = u.SDRC_LINE.SDRC_HARSHAOT.SDRC_MURSHE_MEIDA == "1"
-          val hasMurshePeulot = u.SDRC_LINE.SDRC_HARSHAOT.SDRC_MURSHE_PEULOT == "1"
-          val hasmursheTzadG = u.SDRC_LINE.SDRC_HARSHAOT.SDRC_MURSHE_TZAD_G == "1"
-          result += BasicBankAccount(
-            u.SDRC_LINE.SDRC_CHN.SDRC_CHN_CHN,
-            u.SDRC_LINE.SDRC_CHN.SDRC_CHN_SNIF,
-            u.SDRC_LINE.SDRC_CHN.SDRC_CHN_SUG,
-            jsonExtract.SDR_JONI.MFTOKEN,
-            AccountPermissions(
-                        hasMursheMeida,
-                        hasMurshePeulot,
-                        hasmursheTzadG
-            )
-          ) }
-      }}
-    result.toList
+    val joniMfCall = getJoniMf(username, !useCache)
+    joniMfCall match {
+      case Right(x) =>
+
+        val allowedAccountTypes = config.getString("accountTypes.allowed").split(",").map(_.trim).toList
+        //now extract values from JoniMFUser object into List of BasicBankAccount
+        // (will be changed to inboundBankAccount 2017 objects 
+        var result = new ListBuffer[BasicBankAccount]()
+
+        for (i <- x.SDR_JONI.SDR_LAK_SHEDER.SDRL_LINE) {
+          for (u <- i.SDR_CHN) {
+            if (allowedAccountTypes.contains(u.SDRC_LINE.SDRC_CHN.SDRC_CHN_SUG)) {
+              val hasMursheMeida = u.SDRC_LINE.SDRC_HARSHAOT.SDRC_MURSHE_MEIDA == "1"
+              val hasMurshePeulot = u.SDRC_LINE.SDRC_HARSHAOT.SDRC_MURSHE_PEULOT == "1"
+              val hasmursheTzadG = u.SDRC_LINE.SDRC_HARSHAOT.SDRC_MURSHE_TZAD_G == "1"
+              result += BasicBankAccount(
+                u.SDRC_LINE.SDRC_CHN.SDRC_CHN_CHN,
+                u.SDRC_LINE.SDRC_CHN.SDRC_CHN_SNIF,
+                u.SDRC_LINE.SDRC_CHN.SDRC_CHN_SUG,
+                x.SDR_JONI.MFTOKEN,
+                AccountPermissions(
+                  hasMursheMeida,
+                  hasMurshePeulot,
+                  hasmursheTzadG
+                )
+              )
+            }
+          }
+        }
+        Right(result.toList)
+
+      case Left(x) =>
+        Left(x)
+    }
   }
 
   
--- src/main/scala/com/tesobe/obp/mf_calls/JoniMf.scala
+++ src/main/scala/com/tesobe/obp/mf_calls/JoniMf.scala
@@ -9,6 +9,7 @@ import net.liftweb.json.JsonParser._
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.DefaultHttpClient
+import com.tesobe.obp.HttpClient.makePostRequest
 
 /*//For akka-http
 import scala.concurrent._
@@ -25,14 +26,9 @@ import akka.stream.ActorMaterializer*/
 object JoniMf extends Config with StrictLogging{
 
 
-   def getJoniMfHttpApache(username: String): String = {
+   def getJoniMfCore(username: String): Either[PAPIErrorResponse,JoniMfUser] = {
 
-     val client = new DefaultHttpClient()
-     val url = config.getString("bankserver.url")
-
-     //OBP-Adapter_Leumi/Doc/MFServices/JONI_0_000 Sample.txt
-     val post = new HttpPost(url + "/ESBLeumiDigitalBank/PAPI/V1.0/JONI/0/000/01.01")
-     post.addHeader("Content-Type", "application/json;charset=utf-8")
+     val path = "/ESBLeumiDigitalBank/PAPI/V1.0/JONI/0/000/01.01"
      val json: JValue =parse(s"""
      {
        "JONI_0_000": {
@@ -43,18 +39,66 @@ object JoniMf extends Config with StrictLogging{
          }
        }
      }""")
-       
-     val jsonBody = new StringEntity(compactRender(json), "UTF-8")
-     post.setEntity(jsonBody)
-  
-     logger.debug("JONI_0_000--Request : "+post.toString +"\n Body is :" + compactRender(json))
-     val response = client.execute(post)
-     val inputStream = response.getEntity.getContent
-     val result = scala.io.Source.fromInputStream(inputStream).mkString
-     logger.debug("JONI_0_000--Response : "+response.toString+ "\n Body is :"+result)
-     response.close()
-     result
+
+
+     val result = makePostRequest(json, path)
+     implicit val formats = net.liftweb.json.DefaultFormats
+     try {
+       Right(parse(replaceEmptyObjects(result)).extract[JoniMfUser])
+     } catch {
+       case e: net.liftweb.json.MappingException => Left(parse(replaceEmptyObjects(result)).extract[PAPIErrorResponse])
+     }
    }
+
+  def getJoniMf(username: String, isFirst: Boolean = true) = {
+
+    import scalacache.Flags
+    import scalacache.memoization.{cacheKeyExclude, memoizeSync}
+
+    def getJoniMfCached(username: String)(implicit @cacheKeyExclude flags: Flags): Either[PAPIErrorResponse,JoniMfUser]  = memoizeSync {
+      getJoniMfCore(username)
+    }
+
+    isFirst == true match {
+      case true => // Call MF
+        implicit val flags = Flags(readsEnabled = false)
+        getJoniMfCached(username)
+      case false => // Try to read from cache
+        implicit val flags = Flags(readsEnabled = true)
+        getJoniMfCached(username)
+    }
+  }
+
+  def getJoniMfHttpApache(username: String): String = {
+
+    val client = new DefaultHttpClient()
+    val url = config.getString("bankserver.url")
+
+    //OBP-Adapter_Leumi/Doc/MFServices/JONI_0_000 Sample.txt
+    val post = new HttpPost(url + "/ESBLeumiDigitalBank/PAPI/V1.0/JONI/0/000/01.01")
+    post.addHeader("Content-Type", "application/json;charset=utf-8")
+    val json: JValue =parse(s"""
+     {
+       "JONI_0_000": {
+         "NtdriveCommonHeader": {
+           "AuthArguments": {
+             "UserName": "$username"
+           }
+         }
+       }
+     }""")
+
+    val jsonBody = new StringEntity(compactRender(json), "UTF-8")
+    post.setEntity(jsonBody)
+
+    logger.debug("JONI_0_000--Request : "+post.toString +"\n Body is :" + compactRender(json))
+    val response = client.execute(post)
+    val inputStream = response.getEntity.getContent
+    val result = scala.io.Source.fromInputStream(inputStream).mkString
+    logger.debug("JONI_0_000--Response : "+response.toString+ "\n Body is :"+result)
+    response.close()
+    result
+  }
   
     
    // libweb-json parses the empty object to JObject(List()), but we need JString to extract to String
