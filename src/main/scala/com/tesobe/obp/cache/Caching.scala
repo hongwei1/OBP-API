9c3aa84707849f94e5db42b9b1b6f83215ba2c52
hongwei1 <hhh@tesobe.com>
2018-05-18 21:07:11 +0200

    fix a bug for cacheKey

--- src/main/scala/com/tesobe/obp/cache/Caching.scala
+++ src/main/scala/com/tesobe/obp/cache/Caching.scala
@@ -3,25 +3,24 @@ package com.tesobe.obp.cache
 import com.tesobe.obp.Config
 import scalacache.Flags
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.language.postfixOps
 
 object Caching extends Config{
-
-  def memoizeSyncWithProvider[A](unique: Option[String])(f: => A)(implicit m: Manifest[A], flags: Flags): A = {
-    (unique) match {
-      case (t) if t == Duration.Zero  => // Just forwarding a call
-        f
-      case ( _) => // Caching a call
+  
+  private val zero: FiniteDuration = Duration.Zero
+  def memoizeSyncWithProvider[A](cacheKey: Option[String])(f: => A)(implicit m: Manifest[A], flags: Flags): A = {
+    (cacheKey) match {
+      case Some(_) => // Caching a call
         config.getString("guava.cache.type") match {
           case value if value.toLowerCase == "redis" =>
-            Redis.memoizeSyncWithRedis(unique)(f)
+            Redis.memoizeSyncWithRedis(cacheKey)(f)
           case value if value.toLowerCase == "in-memory" =>
-            InMemory.memoizeSyncWithInMemory(unique)(f)
+            InMemory.memoizeSyncWithInMemory(cacheKey)(f)
           case _ =>
-            InMemory.memoizeSyncWithInMemory(unique)(f)
+            InMemory.memoizeSyncWithInMemory(cacheKey)(f)
         }
-      case _  => // Just forwarding a call
+      case _  => // Just forwarding a call, no caching .
         f
     }
 
