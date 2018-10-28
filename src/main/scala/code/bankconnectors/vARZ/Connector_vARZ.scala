ee5a24862865820be0c09e4c5b06ede341c3d820
hongwei1 <hongwei@tesobe.com>
2018-10-28 18:08:21 +0100

    mocked transactions in Connector_ARZ

--- src/main/scala/code/bankconnectors/vARZ/Connector_vARZ.scala
+++ src/main/scala/code/bankconnectors/vARZ/Connector_vARZ.scala
@@ -28,16 +28,20 @@ import java.util.UUID.randomUUID
 
 import code.api.cache.Caching
 import code.api.util.APIUtil.{MessageDoc, saveConnectorMetric}
-import code.api.util.CallContext
+import code.api.util.{APIUtil, CallContext}
 import code.api.util.ErrorMessages._
-import code.bankconnectors._
+import code.api.v2_1_0.TransactionRequestCommonBodyJSON
+import code.bankconnectors.LocalMappedConnector.{getBankAccount, getTransactionsTTL, saveTransaction, updateAccountTransactions}
+import code.bankconnectors.{OBPQueryParam, _}
 import code.bankconnectors.vARZ.mf_calls._
 import code.bankconnectors.vJune2017.AuthInfo
 import code.bankconnectors.vMar2017._
 import code.customer._
 import code.customer.internalMapping.CustomerIDMappingProvider
+import code.fx.fx
 import code.kafka.KafkaHelper
 import code.model._
+import code.transaction.MappedTransaction
 import code.usercustomerlinks.{MappedUserCustomerLinkProvider, UserCustomerLink}
 import code.users.Users
 import code.util.Helper.MdcLoggable
@@ -45,6 +49,7 @@ import com.tesobe.CacheKeyFromArguments
 import net.liftweb.common.{Box, _}
 import net.liftweb.json.Extraction._
 import net.liftweb.json.JsonAST.JValue
+import net.liftweb.mapper._
 import net.liftweb.util.Helpers.tryo
 
 import scala.collection.immutable.{List, Nil}
@@ -401,9 +406,216 @@ trait Connector_vARZ extends Connector with KafkaHelper with MdcLoggable {
     
     Full((customers, callContext))
   }
+  
+  
+  override def getBankAccount(bankId: BankId, accountId: AccountId, callContext: Option[CallContext])=  {
+    val account = InboundAccountArz(
+            errorCode = "",
+            cbsToken = "",
+            bankId =bankId.value,
+            branchId ="",
+            accountId = accountId.value,
+            accountNumber = "123",
+            accountType = "P",
+            balanceAmount = "123",
+            balanceCurrency = "EUR",
+            owners = List(""),
+            viewsToGenerate = List("owner"),
+            bankRoutingScheme = "",
+            bankRoutingAddress = "",
+            branchRoutingScheme = "",
+            branchRoutingAddress = "",
+            accountRoutingScheme = "",
+            accountRoutingAddress = "",
+            accountRoutings = Nil,
+            accountRules = Nil)
+    Full(BankAccountArz(account), callContext)
+  }
+  
       
+//  override def getTransactionsCore(bankId: BankId, accountID: AccountId, callContext: Option[CallContext], queryParams: OBPQueryParam*): Box[(List[TransactionCore], Option[CallContext])]= {
+//    val account =  getBankAccount(bankId: BankId, accountID: AccountId, callContext: Option[CallContext])
+//    val counterparty = CounterpartyCore(
+//      kind ="",
+//      counterpartyId ="",
+//      counterpartyName ="",
+//      thisBankId =BankId(""), 
+//      thisAccountId =AccountId(""),
+//      otherBankRoutingScheme ="", 
+//      otherBankRoutingAddress = None,
+//      otherAccountRoutingScheme ="", 
+//      otherAccountRoutingAddress = None, 
+//      otherAccountProvider ="", 
+//      isBeneficiary= true 
+//    )
+//
+//    Full((List(
+//      TransactionCore(
+//        id = TransactionId("1231"), 
+//        thisAccount = account.openOrThrowException("No Accounts")._1,
+//        otherAccount = counterparty,
+//        transactionType = "",
+//        amount = BigDecimal("123"), 
+//        currency = "", 
+//        description = Some("house"), 
+//        startDate = new Date(), 
+//        finishDate = new Date(), 
+//        balance = BigDecimal("120"))
+//    ), 
+//         callContext
+//    ))  
+//  }
+//  
+//  
+//  def getTransactions(bankId: BankId, accountID: AccountId, callContext: Option[CallContext], queryParams: OBPQueryParam*): Box[(List[Transaction], Option[CallContext])]=
+  
+  override def getTransactionsCore(bankId: BankId, accountId: AccountId, callContext: Option[CallContext], queryParams: OBPQueryParam*) =
+    {
+
+      // TODO Refactor this. No need for database lookups etc.
+      val limit = queryParams.collect { case OBPLimit(value) => MaxRows[MappedTransaction](value) }.headOption
+      val offset = queryParams.collect { case OBPOffset(value) => StartAt[MappedTransaction](value) }.headOption
+      val fromDate = queryParams.collect { case OBPFromDate(date) => By_>=(MappedTransaction.tFinishDate, date) }.headOption
+      val toDate = queryParams.collect { case OBPToDate(date) => By_<=(MappedTransaction.tFinishDate, date) }.headOption
+      val ordering = queryParams.collect {
+        //we don't care about the intended sort field and only sort on finish date for now
+        case OBPOrdering(_, direction) =>
+          direction match {
+            case OBPAscending => OrderBy(MappedTransaction.tFinishDate, Ascending)
+            case OBPDescending => OrderBy(MappedTransaction.tFinishDate, Descending)
+          }
+      }
+
+      val optionalParams: Seq[QueryParam[MappedTransaction]] = Seq(limit.toSeq, offset.toSeq, fromDate.toSeq, toDate.toSeq, ordering.toSeq).flatten
+      val mapperParams = Seq(By(MappedTransaction.bank, bankId.value), By(MappedTransaction.account, accountId.value)) ++ optionalParams
+
+      def getTransactionsCached(bankId: BankId, accountId: AccountId, optionalParams: Seq[QueryParam[MappedTransaction]]): Box[List[TransactionCore]]
+      = {
+        /**
+          * Please noe that "var cacheKey = (randomUUID().toString, randomUUID().toString, randomUUID().toString)"
+          * is just a temporary value filed with UUID values in order to prevent any ambiguity.
+          * The real value will be assigned by Macro during compile time at this line of a code:
+          * https://github.com/OpenBankProject/scala-macros/blob/master/macros/src/main/scala/com/tesobe/CacheKeyFromArgumentsMacro.scala#L49
+          */
+        var cacheKey = (randomUUID().toString, randomUUID().toString, randomUUID().toString)
+        CacheKeyFromArguments.buildCacheKey {
+          Caching.memoizeSyncWithProvider (Some(cacheKey.toString()))(getTransactionsTTL millisecond) {
+
+          //logger.info("Cache miss getTransactionsCached")
+
+          val mappedTransactions = MappedTransaction.findAll(mapperParams: _*)
+
+          for (account <- getBankAccount(bankId, accountId))
+            yield mappedTransactions.flatMap(_.toTransactionCore(account)) //each transaction will be modified by account, here we return the `class Transaction` not a trait.
+        }
+      }
+    }
+
+    getTransactionsCached(bankId: BankId, accountId: AccountId, optionalParams).map(transactions =>(transactions,callContext))
+  }
+
+  override def getTransactions(bankId: BankId, accountId: AccountId, callContext: Option[CallContext], queryParams: OBPQueryParam*) = {
+
+    // TODO Refactor this. No need for database lookups etc.
+    val limit = queryParams.collect { case OBPLimit(value) => MaxRows[MappedTransaction](value) }.headOption
+    val offset = queryParams.collect { case OBPOffset(value) => StartAt[MappedTransaction](value) }.headOption
+    val fromDate = queryParams.collect { case OBPFromDate(date) => By_>=(MappedTransaction.tFinishDate, date) }.headOption
+    val toDate = queryParams.collect { case OBPToDate(date) => By_<=(MappedTransaction.tFinishDate, date) }.headOption
+    val ordering = queryParams.collect {
+      //we don't care about the intended sort field and only sort on finish date for now
+      case OBPOrdering(_, direction) =>
+        direction match {
+          case OBPAscending => OrderBy(MappedTransaction.tFinishDate, Ascending)
+          case OBPDescending => OrderBy(MappedTransaction.tFinishDate, Descending)
+        }
+    }
+
+    val optionalParams : Seq[QueryParam[MappedTransaction]] = Seq(limit.toSeq, offset.toSeq, fromDate.toSeq, toDate.toSeq, ordering.toSeq).flatten
+    val mapperParams = Seq(By(MappedTransaction.bank, bankId.value), By(MappedTransaction.account, accountId.value)) ++ optionalParams
+
+    def getTransactionsCached(bankId: BankId, accountId: AccountId, optionalParams : Seq[QueryParam[MappedTransaction]]) : Box[List[Transaction]]
+    = {
+      /**
+        * Please noe that "var cacheKey = (randomUUID().toString, randomUUID().toString, randomUUID().toString)"
+        * is just a temporary value filed with UUID values in order to prevent any ambiguity.
+        * The real value will be assigned by Macro during compile time at this line of a code:
+        * https://github.com/OpenBankProject/scala-macros/blob/master/macros/src/main/scala/com/tesobe/CacheKeyFromArgumentsMacro.scala#L49
+        */
+      var cacheKey = (randomUUID().toString, randomUUID().toString, randomUUID().toString)
+      CacheKeyFromArguments.buildCacheKey {
+        Caching.memoizeSyncWithProvider(Some(cacheKey.toString()))(getTransactionsTTL millisecond) {
+
+          //logger.info("Cache miss getTransactionsCached")
+
+          val mappedTransactions = MappedTransaction.findAll(mapperParams: _*)
+
+          for (account <- getBankAccount(bankId, accountId))
+            yield mappedTransactions.flatMap(_.toTransaction(account)) //each transaction will be modified by account, here we return the `class Transaction` not a trait.
+        }
+      }
+    }
+    getTransactionsCached(bankId: BankId, accountId: AccountId, optionalParams).map(transactions => (transactions, callContext))
+  }
+  
+  
+  override def makePaymentImpl(fromAccount: BankAccount,
+                               toAccount: BankAccount,
+                               transactionRequestCommonBody: TransactionRequestCommonBodyJSON,
+                               amount: BigDecimal,
+                               description: String,
+                               transactionRequestType: TransactionRequestType,
+                               chargePolicy: String): Box[TransactionId] = {
+    for{
+       rate <- tryo {fx.exchangeRate(fromAccount.currency, toAccount.currency)} ?~! s"$InvalidCurrency The requested currency conversion (${fromAccount.currency} to ${fromAccount.currency}) is not supported."
+       fromTransAmt = -amount//from fromAccount balance should decrease
+       toTransAmt = fx.convert(amount, rate)
+       sentTransactionId <- saveTransaction(fromAccount, toAccount,transactionRequestCommonBody, fromTransAmt, description, transactionRequestType, chargePolicy)
+       _sentTransactionId <- saveTransaction(toAccount, fromAccount,transactionRequestCommonBody, toTransAmt, description, transactionRequestType, chargePolicy)
+    } yield{
+      sentTransactionId
+    }
+  }
+  private def saveTransaction(fromAccount: BankAccount,
+                              toAccount: BankAccount,
+                              transactionRequestCommonBody: TransactionRequestCommonBodyJSON,
+                              amount: BigDecimal,
+                              description: String,
+                              transactionRequestType: TransactionRequestType,
+                              chargePolicy: String): Box[TransactionId] = {
+    for{
+
+      currency <- Full(fromAccount.currency)
+    //update the balance of the fromAccount for which a transaction is being created
 
 
+      mappedTransaction <- tryo(MappedTransaction.create
+      .bank(fromAccount.bankId.value)
+      .account(fromAccount.accountId.value)
+      .transactionType(transactionRequestType.value)
+      .amount(123)
+      .newAccountBalance(123)
+      .currency(currency)
+      .tStartDate(new Date())
+      .tFinishDate(new Date())
+      .description(description) 
+      .counterpartyAccountHolder(toAccount.accountHolder)
+      .counterpartyAccountNumber(toAccount.number)
+      .counterpartyAccountKind(toAccount.accountType)
+      .counterpartyBankName(toAccount.bankName)
+      .counterpartyIban(toAccount.iban.getOrElse(""))
+      .counterpartyNationalId(toAccount.nationalIdentifier)
+      .CPOtherAccountRoutingScheme(toAccount.accountRoutingScheme)
+      .CPOtherAccountRoutingAddress(toAccount.accountRoutingAddress)
+      .CPOtherBankRoutingScheme(toAccount.bankRoutingScheme)
+      .CPOtherBankRoutingAddress(toAccount.bankRoutingAddress)
+      .chargePolicy(chargePolicy)
+      .saveMe) ?~! s"$CreateTransactionsException, exception happened when create new mappedTransaction"
+    } yield{
+      mappedTransaction.theTransactionId
+    }
+  }
+  
+  
 }
 
 
