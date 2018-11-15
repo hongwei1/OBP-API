75bac71c4c1077e656a9f5142dc59e5078970e43
tawoe <tobias@tesobe.com>
2018-11-15 16:00:18 +0100

    debug getting env variables

--- src/main/scala/bootstrap/liftweb/BootUtil.scala
+++ src/main/scala/bootstrap/liftweb/BootUtil.scala
@@ -1,20 +1,28 @@
 package bootstrap.liftweb
 
+import code.api.util.APIUtil
 import code.util.Helper.MdcLoggable
-import net.liftweb.util.Props.appendProvider
+import net.liftweb.util.Props._
 
-object BootUtil {
+import scala.util.control.NoStackTrace
+
+object BootUtil extends MdcLoggable{
   def getPropsFromOSEnvironment: Unit ={
+    logger.debug("Enter getPropsFromEnvironmentXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     var dbUrl:String = 
       try {
-      sys.env("EBKNG_POSTGRESQL_DATASOURCE_URL" + "?user=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME" +
-        "&password=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_PASSWORD") + "&currentSchema=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME")))
+      sys.env("EBKNG_POSTGRESQL_DATASOURCE_URL") + "?user=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME") +
+        "&password=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_PASSWORD") + "&currentSchema=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME")
     } catch {
       case e: Throwable => 
-          throw new Exception("dbUrl not completely set (must be url, username and password")
+          throw new Exception("dbUrl not completely set (must be url, username and password") with NoStackTrace
           
     }
-     appendProvider(Map("db.url" -> dbUrl))
+    logger.debug("The new db.url is: " + dbUrl)
+     val fred = net.liftweb.util.Props.appendProvider(Map("db.driver" -> "org.postgresql.Driver"))
+     prependProvider(Map("db.url" -> dbUrl))
+    logger.debug("db url props from Environment: " + APIUtil.getPropsValue("db.url"))
+    logger.debug(" with db.driver: " + APIUtil.getPropsValue("db.driver"))
   }
 }
    
