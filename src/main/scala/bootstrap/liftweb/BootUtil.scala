480e0ff0b1dde0db71466a1cd2968677fbaff009
tawoe <tobias@tesobe.com>
2018-11-16 14:39:29 +0100

    refactoring getPropsFromEnvironment

--- src/main/scala/bootstrap/liftweb/BootUtil.scala
+++ src/main/scala/bootstrap/liftweb/BootUtil.scala
@@ -8,19 +8,17 @@ import scala.util.control.NoStackTrace
 
 object BootUtil extends MdcLoggable{
   def getPropsFromOSEnvironment: Unit ={
-    logger.debug("Enter getPropsFromEnvironmentXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     var dbUrl:String = 
       try {
-        sys.env("HOME")
-//      sys.env("EBKNG_POSTGRESQL_DATASOURCE_URL") + "?user=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME") +
-//        "&password=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_PASSWORD") + "&currentSchema=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME")
+      sys.env("EBKNG_POSTGRESQL_DATASOURCE_URL") + "?user=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME") +
+        "&password=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_PASSWORD") + "&currentSchema=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME")
     } catch {
       case e: Throwable => 
           throw new Exception("dbUrl not completely set (must be url, username and password") with NoStackTrace
           
     }
     logger.debug("The new db.url is: " + dbUrl)
-     val fred = net.liftweb.util.Props.prependProvider(Map("db.driver" -> "org.postgresql.Driver"))
+     prependProvider(Map("db.driver" -> "org.postgresql.Driver"))
      prependProvider(Map("db.url" -> dbUrl))
   }
 }
