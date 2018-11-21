1f5cd7b5255e66c533c813e0e73109392b8caed1
tawoe <tobias@tesobe.com>
2018-11-21 20:09:51 +0100

    refactoring db environment

--- src/main/scala/bootstrap/liftweb/BootUtil.scala
+++ src/main/scala/bootstrap/liftweb/BootUtil.scala
@@ -11,7 +11,7 @@ object BootUtil extends MdcLoggable{
     var dbUrl:String = 
       try {
       sys.env("EBKNG_POSTGRESQL_DATASOURCE_URL") + "?user=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME") +
-        "&password=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_PASSWORD") + "&currentSchema=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME")
+        "&password=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_PASSWORD") + "&schema=" + sys.env("EBKNG_POSTGRESQL_DATASOURCE_USERNAME")
     } catch {
       case e: Throwable => 
           throw new Exception("dbUrl not completely set (must be url, username and password") with NoStackTrace
