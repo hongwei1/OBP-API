221abb9bcb66012675a4b145e95957baf4b3b01f
shuang bai <shuang.bai@qq.com>
2019-03-03 19:16:25 +0800

    refs #1244 add cache to metrics endpoints

--- src/main/scala/code/metrics/ConnectorMetrics.scala
+++ src/main/scala/code/metrics/ConnectorMetrics.scala
@@ -3,10 +3,13 @@ package code.metrics
 import java.util.Date
 
 import code.api.util._
-import code.util.MappedUUID
+import code.util.{MappedUUID, TTLCache}
 import net.liftweb.mapper._
 
 object ConnectorMetrics extends ConnectorMetricsProvider {
+
+  val cacheTTL              = APIUtil.getPropsValue("connector.cache.ttl.seconds", "10").toInt
+  val cachedAllConnectorMetrics            = TTLCache[List[MappedConnectorMetric]](cacheTTL)
 
   override def saveConnectorMetric(connectorName: String, functionName: String, correlationId: String, date: Date, duration: Long): Unit = {
     MappedConnectorMetric.create
@@ -16,31 +19,37 @@ object ConnectorMetrics extends ConnectorMetricsProvider {
       .duration(duration)
       .correlationId(correlationId)
       .save
+    cachedAllConnectorMetrics.clear()
   }
 
   override def getAllConnectorMetrics(queryParams: List[OBPQueryParam]): List[MappedConnectorMetric] = {
-    val limit = queryParams.collect { case OBPLimit(value) => MaxRows[MappedConnectorMetric](value) }.headOption
-    val offset = queryParams.collect { case OBPOffset(value) => StartAt[MappedConnectorMetric](value) }.headOption
-    val fromDate = queryParams.collect { case OBPFromDate(date) => By_>=(MappedConnectorMetric.date, date) }.headOption
-    val toDate = queryParams.collect { case OBPToDate(date) => By_<=(MappedConnectorMetric.date, date) }.headOption
-    val correlationId = queryParams.collect { case OBPCorrelationId(value) => By(MappedConnectorMetric.correlationId, value) }.headOption
-    val functionName = queryParams.collect { case OBPFunctionName(value) => By(MappedConnectorMetric.functionName, value) }.headOption
-    val connectorName = queryParams.collect { case OBPConnectorName(value) => By(MappedConnectorMetric.connectorName, value) }.headOption
-    val ordering = queryParams.collect {
-      //we don't care about the intended sort field and only sort on finish date for now
-      case OBPOrdering(_, direction) =>
-        direction match {
-          case OBPAscending => OrderBy(MappedConnectorMetric.date, Ascending)
-          case OBPDescending => OrderBy(MappedConnectorMetric.date, Descending)
-        }
-    }
-    val optionalParams : Seq[QueryParam[MappedConnectorMetric]] = Seq(limit.toSeq, offset.toSeq, fromDate.toSeq, toDate.toSeq, ordering, 
-                                                                      correlationId.toSeq, functionName.toSeq, connectorName.toSeq).flatten
+    val cacheKey = queryParams.map(String.valueOf(_)).mkString(",")
 
-    MappedConnectorMetric.findAll(optionalParams: _*)
+    this.cachedAllConnectorMetrics.getOrElseUpdate(cacheKey, () => {
+      val limit = queryParams.collect { case OBPLimit(value) => MaxRows[MappedConnectorMetric](value) }.headOption
+      val offset = queryParams.collect { case OBPOffset(value) => StartAt[MappedConnectorMetric](value) }.headOption
+      val fromDate = queryParams.collect { case OBPFromDate(date) => By_>=(MappedConnectorMetric.date, date) }.headOption
+      val toDate = queryParams.collect { case OBPToDate(date) => By_<=(MappedConnectorMetric.date, date) }.headOption
+      val correlationId = queryParams.collect { case OBPCorrelationId(value) => By(MappedConnectorMetric.correlationId, value) }.headOption
+      val functionName = queryParams.collect { case OBPFunctionName(value) => By(MappedConnectorMetric.functionName, value) }.headOption
+      val connectorName = queryParams.collect { case OBPConnectorName(value) => By(MappedConnectorMetric.connectorName, value) }.headOption
+      val ordering = queryParams.collect {
+        //we don't care about the intended sort field and only sort on finish date for now
+        case OBPOrdering(_, direction) =>
+          direction match {
+            case OBPAscending => OrderBy(MappedConnectorMetric.date, Ascending)
+            case OBPDescending => OrderBy(MappedConnectorMetric.date, Descending)
+          }
+      }
+      val optionalParams : Seq[QueryParam[MappedConnectorMetric]] = Seq(limit.toSeq, offset.toSeq, fromDate.toSeq, toDate.toSeq, ordering,
+                                                                        correlationId.toSeq, functionName.toSeq, connectorName.toSeq).flatten
+
+      MappedConnectorMetric.findAll(optionalParams: _*)
+    })
   }
 
   override def bulkDeleteConnectorMetrics(): Boolean = {
+    cachedAllConnectorMetrics.clear()
     MappedMetric.bulkDelete_!!()
   }
 
