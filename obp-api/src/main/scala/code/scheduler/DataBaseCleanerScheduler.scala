7a79702abf4fa803bbb7d50e46eacc97410c0450
hongwei <hongwei@tesobe.com>
2026-06-14 10:03:17 +0200

    feat: migrate DataBaseCleanerScheduler and TransactionScheduler to Doobie
    Replace JobScheduler Lift Mapper calls in DataBaseCleanerScheduler with direct
    Doobie SQL (INSERT with returning id, SELECT, DELETE).
    
    Replace MappedTransactionRequest findAll + .mStatus.save in TransactionScheduler
    with Doobie SELECT and UPDATE on mappedtransactionrequest.

--- obp-api/src/main/scala/code/scheduler/DataBaseCleanerScheduler.scala
+++ obp-api/src/main/scala/code/scheduler/DataBaseCleanerScheduler.scala
@@ -2,17 +2,17 @@ package code.scheduler
 
 import code.actorsystem.ObpActorSystem
 import code.api.Constant
-import code.api.util.APIUtil.generateUUID
-import code.api.util.APIUtil
+import code.api.util.{APIUtil, DoobieUtil}
 import code.nonce.Nonces
+import code.token.Tokens
 import code.util.Helper.MdcLoggable
-import net.liftweb.common.Full
-import net.liftweb.mapper.{By, By_<=}
+import doobie._
+import doobie.implicits._
 
+import java.sql.Timestamp
 import java.util.concurrent.TimeUnit
 import java.util.Date
 import scala.concurrent.duration._
-import code.token.Tokens
 
 
 object DataBaseCleanerScheduler extends MdcLoggable {
@@ -25,43 +25,50 @@ object DataBaseCleanerScheduler extends MdcLoggable {
   private val jobName = DataBaseCleanerScheduler.getClass.getSimpleName.replace("$", "")
   private val apiInstanceId = Constant.ApiInstanceId
 
+  private case class JobRow(id: Long, jobId: String)
+
   def start(intervalInSeconds: Long): Unit = {
     logger.info(s"Hello from $jobName.start")
 
     logger.info(s"--------- Clean up Jobs ---------")
     logger.info(s"Delete all Jobs created by api_instance_id=$apiInstanceId")
-    JobScheduler.findAll(By(JobScheduler.Name, apiInstanceId)).map { i =>
-      logger.info(s"Job name: ${i.name}, Date: ${i.createdAt}")
-      i
-    }.map(_.delete_!)
+    // Preserve original query (By Name = apiInstanceId) for bug-for-bug compatibility.
+    DoobieUtil.runQuery(
+      sql"DELETE FROM jobscheduler WHERE name = $apiInstanceId".update.run
+    )
+
     logger.info(s"Delete all Jobs older than 5 days")
     val fiveDaysAgo: Date = new Date(new Date().getTime - (oneDayInMillis * 5))
-    JobScheduler.findAll(By_<=(JobScheduler.createdAt, fiveDaysAgo)).map { i =>
-      logger.info(s"Job name: ${i.name}, Date: ${i.createdAt}, api_instance_id: ${apiInstanceId}")
-      i
-    }.map(_.delete_!)
-    
+    val fiveDaysAgoTs = new Timestamp(fiveDaysAgo.getTime)
+    DoobieUtil.runQuery(
+      sql"DELETE FROM jobscheduler WHERE createdat <= $fiveDaysAgoTs".update.run
+    )
+
     scheduler.schedule(
       initialDelay = Duration(intervalInSeconds, TimeUnit.SECONDS),
       interval = Duration(intervalInSeconds, TimeUnit.SECONDS),
       runnable = new Runnable {
         def run(): Unit = {
-          JobScheduler.find(By(JobScheduler.Name, jobName)) match {
-            case Full(job) => // There is an ongoing/hanging job
-              logger.info(s"Cannot start $jobName.start.run due to ongoing job. Job ID: ${job.JobId}")
-            case _ => // Start a new job
-              val uniqueId = generateUUID()
-              val job = JobScheduler.create
-                .JobId(uniqueId)
-                .Name(jobName)
-                .ApiInstanceId(apiInstanceId)
-                .saveMe()
+          DoobieUtil.runQuery(
+            fr"SELECT id, jobid FROM jobscheduler WHERE name = $jobName LIMIT 1"
+              .query[JobRow].option
+          ) match {
+            case Some(job) =>
+              logger.info(s"Cannot start $jobName.start.run due to ongoing job. Job ID: ${job.jobId}")
+            case None =>
+              val uniqueId = APIUtil.generateUUID()
+              val now = new Timestamp(System.currentTimeMillis())
+              val insertedId: Long = DoobieUtil.runQuery(
+                sql"""INSERT INTO jobscheduler (jobid, name, apiinstanceid, createdat, updatedat)
+                      VALUES ($uniqueId, $jobName, $apiInstanceId, $now, $now)"""
+                  .update.withUniqueGeneratedKeys[Long]("id")
+              )
               logger.info(s"Starting $jobName.Job ID: $uniqueId")
               deleteExpiredTokensAndNonces()
-              JobScheduler.delete_!(job) // Allow future jobs
+              DoobieUtil.runQuery(sql"DELETE FROM jobscheduler WHERE id = $insertedId".update.run)
               logger.info(s"End of $jobName.Job ID: $uniqueId")
           }
-        } 
+        }
       }
     )
     logger.info(s"Bye from $jobName.start")
@@ -77,4 +84,3 @@ object DataBaseCleanerScheduler extends MdcLoggable {
 
 
 }
-
