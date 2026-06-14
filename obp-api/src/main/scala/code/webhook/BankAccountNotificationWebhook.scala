b156c94c463be6c629dfe877f711711bcda15ef8
hongwei <hongwei@tesobe.com>
2026-06-14 10:21:16 +0200

    feat: migrate account webhook providers to Doobie

--- obp-api/src/main/scala/code/webhook/BankAccountNotificationWebhook.scala
+++ obp-api/src/main/scala/code/webhook/BankAccountNotificationWebhook.scala
@@ -4,43 +4,53 @@ import code.api.util._
 import code.util.{AccountIdString, MappedUUID, UUIDString}
 import net.liftweb.common.{Box, Full}
 import net.liftweb.mapper._
-import net.liftweb.util.Helpers.tryo
+import doobie._
+import doobie.implicits._
 
+import java.sql.Timestamp
 import scala.collection.immutable.List
 import com.openbankproject.commons.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+private case class BankAccountNotificationWebhookRow(
+  webhookId: String,
+  bankId: String,
+  triggerName: String,
+  url: String,
+  httpMethod: String,
+  httpProtocol: String,
+  createdByUserId: String
+) extends BankAccountNotificationWebhookTrait
+
 object MappedBankAccountNotificationWebhookProvider extends BankAccountNotificationWebhookProvider {
-  
-  override def getBankAccountNotificationWebhookByIdFuture(webhookId: String): Future[Box[BankAccountNotificationWebhookTrait]] = {
-    Future(
-      BankAccountNotificationWebhook.find(
-        By(BankAccountNotificationWebhook.WebhookId, webhookId)
-      )
-    )
+
+  override def getBankAccountNotificationWebhookByIdFuture(webhookId: String): Future[Box[BankAccountNotificationWebhookTrait]] = Future {
+    DoobieUtil.runQuery(
+      fr"SELECT webhookid, bankid, triggername, url, httpmethod, httpprotocol, createdbyuserid FROM bankaccountnotificationwebhook WHERE webhookid = $webhookId"
+        .query[BankAccountNotificationWebhookRow].option
+    ) match {
+      case Some(row) => Full(row)
+      case None      => net.liftweb.common.Empty
+    }
   }
-  
-  override def getBankAccountNotificationWebhooksByUserIdFuture(userId: String): Future[Box[List[BankAccountNotificationWebhookTrait]]] = {
-    Future(
-      Full(
-        BankAccountNotificationWebhook.findAll(
-          By(BankAccountNotificationWebhook.CreatedByUserId, userId),
-          OrderBy(BankAccountNotificationWebhook.updatedAt, Descending)
-        )
-      )
-    )
+
+  override def getBankAccountNotificationWebhooksByUserIdFuture(userId: String): Future[Box[List[BankAccountNotificationWebhookTrait]]] = Future {
+    Full(DoobieUtil.runQuery(
+      fr"SELECT webhookid, bankid, triggername, url, httpmethod, httpprotocol, createdbyuserid FROM bankaccountnotificationwebhook WHERE createdbyuserid = $userId ORDER BY updatedat DESC"
+        .query[BankAccountNotificationWebhookRow].to[List]
+    ))
   }
-  
-  override def getBankAccountNotificationWebhooksFuture(queryParams: List[OBPQueryParam]): Future[Box[List[BankAccountNotificationWebhookTrait]]] = {
-    val limit = queryParams.collectFirst { case OBPLimit(value) => MaxRows[BankAccountNotificationWebhook](value) }
-    val offset = queryParams.collectFirst { case OBPOffset(value) => StartAt[BankAccountNotificationWebhook](value) }
-    val userId = queryParams.collectFirst { case OBPUserId(value) => By(BankAccountNotificationWebhook.CreatedByUserId, value) }
-    val optionalParams: Seq[QueryParam[BankAccountNotificationWebhook]] = Seq(limit.toSeq, offset.toSeq, userId.toSeq).flatten
-    Future(
-      Full(
-        BankAccountNotificationWebhook.findAll(optionalParams: _*)
-      )
-    )
+
+  override def getBankAccountNotificationWebhooksFuture(queryParams: List[OBPQueryParam]): Future[Box[List[BankAccountNotificationWebhookTrait]]] = Future {
+    val limit  = queryParams.collectFirst { case OBPLimit(v)  => v }.getOrElse(100)
+    val offset = queryParams.collectFirst { case OBPOffset(v) => v }.getOrElse(0)
+    val userId = queryParams.collectFirst { case OBPUserId(v) => v }
+    val baseQ  = fr"SELECT webhookid, bankid, triggername, url, httpmethod, httpprotocol, createdbyuserid FROM bankaccountnotificationwebhook"
+    val whereQ = userId.map(u => fr"WHERE createdbyuserid = $u").getOrElse(fr"")
+    Full(DoobieUtil.runQuery(
+      (baseQ ++ whereQ ++ fr"LIMIT $limit OFFSET $offset")
+        .query[BankAccountNotificationWebhookRow].to[List]
+    ))
   }
 
   override def createBankAccountNotificationWebhookFuture(
@@ -50,20 +60,24 @@ object MappedBankAccountNotificationWebhookProvider extends BankAccountNotificat
     url: String,
     httpMethod: String,
     httpProtocol: String,
-  ): Future[Box[BankAccountNotificationWebhookTrait]] = {
-    val createBankAccountNotificationWebhook = BankAccountNotificationWebhook.create
-      .BankId(bankId)
-      .CreatedByUserId(userId)
-      .TriggerName(triggerName)
-      .Url(url)
-      .HttpMethod(httpMethod)
-      .HttpProtocol(httpProtocol)
-      .saveMe()
-    Future(Full(createBankAccountNotificationWebhook))
+  ): Future[Box[BankAccountNotificationWebhookTrait]] = Future {
+    val webhookId = APIUtil.generateUUID()
+    val now       = new Timestamp(System.currentTimeMillis())
+    DoobieUtil.runQuery(
+      sql"""INSERT INTO bankaccountnotificationwebhook
+              (webhookid, bankid, createdbyuserid, triggername, url, httpmethod, httpprotocol, createdat, updatedat)
+            VALUES
+              ($webhookId, $bankId, $userId, $triggerName, $url, $httpMethod, $httpProtocol, $now, $now)"""
+        .update.run
+    )
+    Full(BankAccountNotificationWebhookRow(webhookId, bankId, triggerName, url, httpMethod, httpProtocol, userId))
   }
 
-  override def deleteBankAccountNotificationWebhookFuture(webhookId: String): Future[Box[Boolean]] = {
-    Future{BankAccountNotificationWebhook.find(By(BankAccountNotificationWebhook.WebhookId, webhookId)).map(_.delete_!)}
+  override def deleteBankAccountNotificationWebhookFuture(webhookId: String): Future[Box[Boolean]] = Future {
+    val count = DoobieUtil.runQuery(
+      sql"DELETE FROM bankaccountnotificationwebhook WHERE webhookid = $webhookId".update.run
+    )
+    Full(count > 0)
   }
 
 }
